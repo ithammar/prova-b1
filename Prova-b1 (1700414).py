b30e5a0a74d2440ed8426ab2e40b0795c3348255
ithammar <30834087+ithammar@users.noreply.github.com>
2017-09-27 11:32:45 -0300

    Atividade valendo nota da B1 (1700414)

---
+++ Prova-b1 (1700414).py
@@ -0,0 +1,151 @@
+ip =  ['192.168.1.0'
+    ,'0.168.1.31'
+    ,'192.168.1.32'
+    ,'192.168.1.63'
+    ,'-123.168.1.64' 
+    ,'-192.168.1.95'
+    ,'192.168.1.127'
+    ,'192.168.1.159'
+    ,'192.168.1.192'
+    ,'192.168.1.223'
+    ,'008.168.1.255'
+    ,'192.168.1.1'
+    ,'192.168.1.33'
+    ,'192.168.1.65'
+    ,'192.168.1.97'
+    ,'192.168.1.129'
+    ,'355.168.1.161'
+    ,'192.168.1.193'
+    ,'192.168.1.225'
+    ,'-798.168.1.128'
+    ,'192.168.1.191'
+    ,'192.168.1.224'
+    ,'192.168.1.96']
+
+valido = []
+invalido = []
+
+if len(ip[0][0:3])> 1: 
+    valido = ip[0]
+else:
+    invalido = ip[0]
+
+if len(ip[1][0:3])> 1: 
+        valido = ip[1]
+else:
+        invalido = ip[1]
+
+if len(ip[2][0:3])> 1: 
+        valido = ip[2]
+else:
+        invalido = ip[2]
+
+if len(ip[3][0:3])> 1: 
+        valido = ip[3]
+else:
+        invalido = ip[3]
+
+if len(ip[4][0:3])> 1: 
+        valido = ip[4]
+else:
+        invalido = ip[4]
+
+if len(ip[5][0:3])> 1: 
+        valido = ip[5]
+else:
+        invalido = ip[5]
+
+if len(ip[6][0:3])> 1: 
+        valido = ip[6]
+else:
+        invalido = ip[6]
+
+if len(ip[7][0:3])> 1: 
+        valido = ip[7]
+else:
+        invalido = ip[7]
+
+if len(ip[8][0:3])> 1: 
+        valido = ip[8]
+else:
+        invalido = ip[8]
+
+if len(ip[9][0:3])> 1: 
+        valido = ip[9]
+else:
+        invalido = ip[9]
+
+if len(ip[10][0:3])> 1: 
+        valido = ip[10]
+else:
+        invalido = ip[10]
+
+if len(ip[11][0:3])> 1: 
+        valido = ip[11]
+else:
+        invalido = ip[11]
+
+if len(ip[12][0:3])> 1: 
+        valido = ip[12]
+else:
+        invalido = ip[12]
+
+if len(ip[13][0:3])> 1: 
+        valido = ip[13]
+else:
+        invalido = ip[13]
+
+if len(ip[14][0:3])> 1: 
+        valido = ip[14]
+else:
+        invalido = ip[14]
+
+if len(ip[15][0:3])> 1: 
+        valido = ip[15]
+else:
+        invalido = ip[15]
+
+if len(ip[16][0:3])> 1: 
+        valido = ip[16]
+else:
+        invalido = ip[16]
+
+if len(ip[17][0:3])> 1: 
+        valido = ip[17]
+else:
+        invalido = ip[17]
+
+if len(ip[18][0:3])> 1: 
+        valido = ip[18]
+else:
+        invalido = ip[18]
+
+if len(ip[19][0:3])> 1: 
+        valido = ip[19]
+else:
+        invalido = ip[19]
+
+if len(ip[20][0:3])> 1: 
+        valido = ip[20]
+else:
+        invalido = ip[20]
+
+if len(ip[21][0:3])> 1: 
+        valido = ip[21]
+else:
+        invalido = ip[21]
+
+if len(ip[22][0:3])> 1: 
+        valido = ip[22]
+else:
+        invalido = ip[22]
+
+
+print("Ip válido", valido)
+print("Ip inválido", invalido)
+
+
+
+
+    
+
